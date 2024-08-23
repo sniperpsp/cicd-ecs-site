679c90cfa9a44fff892f1bec65ab92ee8f0a0e60
Djair Silva <djavasilva@yahoo.com.br>
2024-08-23 12:33:57 -0300

    Fargate v10

--- update_dns.sh
+++ update_dns.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
 # Configurações
-CLUSTER_NAME="cluster-todo"
-SERVICE_NAME="service-todo"
 HOSTED_ZONE_ID="Z09315943W3HUYZ1CG61C"
 DNS_NAME="banco-todo.trustcompras.com.br"
 
-# Obter o ID da tarefa ECS
-TASK_ARN=$(aws ecs list-tasks --cluster $CLUSTER_NAME --service-name $SERVICE_NAME --query 'taskArns[0]' --output text)
-
-# Verificar se o TASK_ARN é válido
-if [ -z "$TASK_ARN" ]; then
-  echo "Erro: Não foi possível obter o ID da tarefa ECS."
-  exit 1
-fi
-
-# Obter o IP do container/task
-TASK_IP=$(aws ecs describe-tasks --cluster $CLUSTER_NAME --tasks $TASK_ARN --query 'tasks[0].attachments[0].details[?name==`privateIPv4Address`].value' --output text)
+# Obter o IP público do container
+TASK_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
 # Verificar se o TASK_IP é um IPv4 válido
 if [[ ! $TASK_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
