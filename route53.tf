5a22fbd0d50ad100e6c08a71aca7a54bbcc532ac
Djair Silva <djavasilva@yahoo.com.br>
2024-08-23 12:05:19 -0300

    Fargate v9

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "app" {
   zone_id = "Z09315943W3HUYZ1CG61C"  # ID da zona existente
-  name    = "banco-todo.trustcompras.com.br"
+  name    = "app-todo.trustcompras.com.br"
   type    = "A"
 
   alias {
@@ -8,4 +8,18 @@ resource "aws_route53_record" "app" {
     zone_id                = aws_lb.app_lb.zone_id
     evaluate_target_health = true
   }
+}
+
+data "aws_ecs_task" "task_todo" {
+  cluster = aws_ecs_cluster.cluster_todo.id
+  task_id = aws_ecs_service.service_todo.task_definition
+}
+
+resource "aws_route53_record" "db" {
+  zone_id = "Z09315943W3HUYZ1CG61C"  # ID da zona existente
+  name    = "banco-todo.trustcompras.com.br"
+  type    = "A"
+  ttl     = 60
+
+  records = [data.aws_ecs_task.task_todo.network_interface[0].private_ip]
 }
