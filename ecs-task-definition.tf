6d3193e53dad815e181ff3e3d38046a6cd762779
Djair Silva <djavasilva@yahoo.com.br>
2024-08-23 08:51:52 -0300

    FargateV4

--- ecs-task-definition.tf
+++ ecs-task-definition.tf
@@ -56,31 +56,3 @@ resource "aws_ecs_task_definition" "task_todo" {
     }
   ])
 }
-
-resource "aws_ecs_service" "task_todo" {
-  name            = "service-todo-${random_id.suffix.hex}"
-  cluster         = aws_ecs_cluster.cluster_todo.id
-  task_definition = aws_ecs_task_definition.task_todo.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-    security_groups  = [aws_security_group.SG1.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.node_todo.arn
-    container_name   = "todo-app"
-    container_port   = 8080
-  }
-
-  depends_on = [
-    aws_lb_listener.lb_listener_https
-  ]
-}
-
-resource "random_id" "suffix" {
-  byte_length = 4
-}
